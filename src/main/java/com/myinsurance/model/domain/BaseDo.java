8a31ca2907d47a1157e8500d4d9609f50bccbef8
berry <2191607562@qq.com>
2018-10-15 15:35:24 +0800

    [add]添加dao和实体类。

---
+++ src/main/java/com/myinsurance/model/domain/BaseDo.java
@@ -0,0 +1,6 @@
+package com.myinsurance.model.domain;
+
+import java.io.Serializable;
+
+public class BaseDo implements Serializable {
+}
---
+++ src/main/java/com/myinsurance/utils/ExceptionUtil.java
@@ -0,0 +1,13 @@
+package com.myinsurance.utils;
+
+public class ExceptionUtil {
+
+    public static String getExceptionAllinformation(Exception ex){
+        String sOut = "";
+        StackTraceElement[] trace = ex.getStackTrace();
+        for (StackTraceElement s : trace) {
+            sOut += "\tat " + s + "\r\n";
+        }
+        return sOut;
+    }
+}
